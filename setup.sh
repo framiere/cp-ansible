18e4e24efcc0dd5cf1f0cc577bb60d6b22858b44
Florent Ramière <florent@ramiere.com>
2018-10-08 17:10:06 +0200

    cosmetics

--- setup.sh
+++ setup.sh
@@ -3,4 +3,5 @@
 vagrant init
 vagrant plugin install vagrant-hostmanager vagrant-vbguest
 vagrant up
+cat .vagrant/provisioners/ansible/inventory/vagrant_ansible_inventory
 ansible-playbook -i hosts.yml all.yml -v
